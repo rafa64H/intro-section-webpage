890d20bd0601a7f9f1c040ebf523912cdd771bbf
Zarknolen <rafarknolen@gmail.com>
2022-06-13 16:11:30 -0400

    I added background color for the rest of the page when opening nav bar and a better style, now it is needed to program the dropdown menu

--- js/index.js
+++ js/index.js
@@ -1,18 +1,37 @@
 let buttonOpenCloseNav = document.querySelector('.o-close-nav-btn')
 let navigationBar = document.querySelector('nav')
+let dropdown = document.querySelector('.dropdown')
+let overlay = document.querySelector('#overlay')
 
 buttonOpenCloseNav.addEventListener('click', () => {
 
-	if (navigationBar.dataset.toggleStatus == 'true'){
-		navigationBar.dataset.toggleStatus = 'false'
-	}
+	switch (navigationBar.dataset.toggleStatus){
+		case 'true':
+			navigationBar.dataset.toggleStatus = 'false'
 
-	else if (navigationBar.dataset.toggleStatus == 'false'){
-		navigationBar.dataset.toggleStatus = 'true'
-	} 
+			overlay.dataset.overlayActive = 'false'
 
-	else if (navigationBar.dataset.toggleStatus == ''){
-		navigationBar.dataset.toggleStatus = 'true'
+			buttonOpenCloseNav.src="images/icon-menu.svg"
+
+			break
+
+		case 'false':
+			navigationBar.dataset.toggleStatus = 'true'
+
+			overlay.dataset.overlayActive = 'true'
+
+			buttonOpenCloseNav.src="images/icon-close-menu.svg"
+
+			break
+
+		default: /*For the beginning value*/
+			navigationBar.dataset.toggleStatus = 'true'
+
+			overlay.dataset.overlayActive = 'true'
+
+			buttonOpenCloseNav.src="images/icon-close-menu.svg"
+
+			break
 	}
 
 })
