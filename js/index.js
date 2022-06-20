7499747c148b035fd926014ec10d0a5cd2fc4c70
Zarknolen <rafarknolen@gmail.com>
2022-06-20 17:06:51 -0400

    I almost finished the Header (Navigation bar, logo, and the button to open navigation bar in mobiles), but it needs the font.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,24 @@
 let buttonOpenCloseNav = document.querySelector('.o-close-nav-btn')
+
 let navigationBar = document.querySelector('nav')
+
+
 let OpenOrCloseDropdown = document.querySelectorAll('.open-close-dropdown')
-let OpenOrCloseDropdownArrows = document.querySelectorAll('.drop-arrow')
+
+let OpenOrCloseDropdownArrowIcons = document.querySelectorAll('.drop-arrow')
+
 let dropdowns = document.querySelectorAll('.dropdown')
+
+
 let overlay = document.querySelector('#overlay')
+
 
 buttonOpenCloseNav.addEventListener('click', () => {
 
 	switch (navigationBar.dataset.navToggleStatus){
+
 		case 'true':
+
 			navigationBar.dataset.navToggleStatus = 'false'
 
 			overlay.dataset.overlayActive = 'false'
@@ -17,7 +27,9 @@ buttonOpenCloseNav.addEventListener('click', () => {
 
 			break
 
+
 		case 'false':
+
 			navigationBar.dataset.navToggleStatus = 'true'
 
 			overlay.dataset.overlayActive = 'true'
@@ -26,7 +38,9 @@ buttonOpenCloseNav.addEventListener('click', () => {
 
 			break
 
-		default: /*For the beginning value*/
+
+		default: /*For the beginning value (to open)*/
+
 			navigationBar.dataset.navToggleStatus = 'true'
 
 			overlay.dataset.overlayActive = 'true'
@@ -34,35 +48,65 @@ buttonOpenCloseNav.addEventListener('click', () => {
 			buttonOpenCloseNav.src="images/icon-close-menu.svg"
 
 			break
+
 	}
 
 })
 
-OpenOrCloseDropdown.forEach(showDropmenu => {
 
-	showDropmenu.addEventListener('click', (e) => {
+OpenOrCloseDropdown.forEach(elementsWithDropdownClasses => {
+
+	elementsWithDropdownClasses.addEventListener('click', (e) => {
 		makeDropMenuAppear(e);
 	})
 })
 
 function makeDropMenuAppear(e){
+
 	if (e.target === OpenOrCloseDropdown[0]){
 
-		switch (dropdowns[0].dataset.dropActive){
+		switch (dropdowns[0].dataset.dropActive){ 
 
 			case 'true':
+
 				dropdowns[0].dataset.dropActive = 'false'
-				OpenOrCloseDropdownArrows[0].dataset.dropArrowActive = 'false'
+
+				OpenOrCloseDropdownArrowIcons[0].dataset.dropArrowActive = 'false'
+
 				break;
+
 
 			case 'false':
+
+				if(dropdowns[1].dataset.dropActive === 'true'){
+
+					dropdowns[1].dataset.dropActive = 'false'
+
+					OpenOrCloseDropdownArrowIcons[1].dataset.dropArrowActive = 'false'
+
+				}
+
 				dropdowns[0].dataset.dropActive = 'true'
-				OpenOrCloseDropdownArrows[0].dataset.dropArrowActive = 'true'
+
+				OpenOrCloseDropdownArrowIcons[0].dataset.dropArrowActive = 'true'
+
 				break;
 
-			default:
+
+			default: /*For the beginning value (to open)*/
+
+				if(dropdowns[1].dataset.dropActive === 'true'){
+
+					dropdowns[1].dataset.dropActive = 'false'
+
+					OpenOrCloseDropdownArrowIcons[1].dataset.dropArrowActive = 'false'
+
+				}
+
 				dropdowns[0].dataset.dropActive = 'true'
-				OpenOrCloseDropdownArrows[0].dataset.dropArrowActive = 'true'
+
+				OpenOrCloseDropdownArrowIcons[0].dataset.dropArrowActive = 'true'
+
 				break;
 
 		}
@@ -73,18 +117,45 @@ function makeDropMenuAppear(e){
 		switch (dropdowns[1].dataset.dropActive){
 
 			case 'true':
+
 				dropdowns[1].dataset.dropActive = 'false'
-				OpenOrCloseDropdownArrows[1].dataset.dropArrowActive = 'false'
+
+				OpenOrCloseDropdownArrowIcons[1].dataset.dropArrowActive = 'false'
+
 				break;
+
 
 			case 'false':
+
+				if(dropdowns[0].dataset.dropActive === 'true'){
+
+					dropdowns[0].dataset.dropActive = 'false'
+
+					OpenOrCloseDropdownArrowIcons[0].dataset.dropArrowActive = 'false'
+
+				}
+
 				dropdowns[1].dataset.dropActive = 'true'
-				OpenOrCloseDropdownArrows[1].dataset.dropArrowActive = 'true'
+
+				OpenOrCloseDropdownArrowIcons[1].dataset.dropArrowActive = 'true'
+
 				break;
 
-			default:
+
+			default: /*For the beginning value (to open)*/
+
+				if(dropdowns[0].dataset.dropActive === 'true'){
+
+					dropdowns[0].dataset.dropActive = 'false'
+
+					OpenOrCloseDropdownArrowIcon[0].dataset.dropArrowActive = 'false'
+
+				}
+
 				dropdowns[1].dataset.dropActive = 'true'
-				OpenOrCloseDropdownArrows[1].dataset.dropArrowActive = 'true'
+
+				OpenOrCloseDropdownArrowIcons[1].dataset.dropArrowActive = 'true'
+
 				break;
 
 		}
